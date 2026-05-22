b67dae595ba1f0811cf35d8628157b925bfcdc16
cardenascamilo-dot <cardenas.camilo@maximino.edu.co>
2026-05-22 11:57:30 -0500

    Update movement.py

--- DiscordBotProject/game/movement.py
+++ DiscordBotProject/game/movement.py
@@ -1,17 +1,30 @@
 # the main code for moving around the map
 
+from discord import app_commands
+import discord
+
 from game.player import players, create_player
 from game.movement import move_player
 
-GAME_ROLE_ID = 123456789012345678 #i dont have my pc right now to get the role i gotta change this
+GAME_ROLE_ID = 123456789012345678  # replace with your role id
+
 
 @bot.tree.command(name="move", description="Move through the train")
-@app_commands.describe(direction="front or back")
-async def move(interaction: discord.Interaction, direction: str):
+@app_commands.describe(direction="Choose where to move")
+
+@app_commands.choices(direction=[
+    app_commands.Choice(name="Front", value="front"),
+    app_commands.Choice(name="Back", value="back")
+])
+
+async def move(
+    interaction: discord.Interaction,
+    direction: app_commands.Choice[str]
+):
 
     member = interaction.user
 
-    # Check role
+    # check if player has the passenger role
     has_role = any(role.id == GAME_ROLE_ID for role in member.roles)
 
     if not has_role:
@@ -23,10 +36,15 @@ async def move(interaction: discord.Interaction, direction: str):
 
     user_id = str(member.id)
 
+    # create player if they existn't
     if user_id not in players:
         create_player(user_id)
 
-    result = move_player(players[user_id], direction)
+    # move player
+    result = move_player(
+        players[user_id],
+        direction.value
+    )
 
     if result is None:
         await interaction.response.send_message(
