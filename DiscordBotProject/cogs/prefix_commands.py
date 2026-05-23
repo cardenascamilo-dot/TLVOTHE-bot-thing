8615e965a7819085d2596f02212fa958f4b3237b
notebarthendew-crypto <notebarthendew@gmail.com>
2026-05-22 20:49:53 -0500

    Update prefix_commands.py

--- DiscordBotProject/cogs/prefix_commands.py
+++ DiscordBotProject/cogs/prefix_commands.py
@@ -1,1 +1,12 @@
+from discord import app_commands
+import discord
 
+def setup_commands(bot):
+
+    @bot.command()
+    async def hello(ctx):
+        await ctx.send("hi")
+
+    @bot.command()
+    async def cheese(ctx):
+        await ctx.send("cheese")
